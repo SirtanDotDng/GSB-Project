d3129588cf4dfd76654081fc5401706854b47bba
SirtanDotDng <94062671+SirtanDotDng@users.noreply.github.com>
2022-02-22 16:44:52 +0100

    inutilisés

--- pages/login.php
+++
@@ -1,9 +0,0 @@
-<?php 
-
-$mail = $_POST['mail'];
-$password = $_POST['pass'];
-
-require('../includes/fonctions.php');
-login($mail, $password);
-
-?>
--- pages/register.php
+++
@@ -1,7 +0,0 @@
-<?php include('../includes/bd.php'); ?>
-
-<?php 
-
-$mail = $_POST['mail'];
-$password = $_POST['password'];
-
